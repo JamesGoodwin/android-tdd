dff744af6471709fc5953c30eb2aa9498cb5d4a3
James Goodwin <jamesgoodwin@gmail.com>
2015-01-15 10:22:21 +0000

    Upgrade to Espresso 2.0

--- app/src/test/java/com/example/espresso/activity/DeckardEspressoTest.java
+++ app/src/test/java/com/example/espresso/activity/DeckardEspressoTest.java
@@ -5,19 +5,20 @@ import android.test.suitebuilder.annotation.LargeTest;
 import com.example.R;
 import com.example.robolectric.DeckardActivity;
 
-import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
-import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @LargeTest
 public class DeckardEspressoTest extends ActivityInstrumentationTestCase2<DeckardActivity> {
 
     @SuppressWarnings("deprecation")
-     public DeckardEspressoTest() {
-       // This constructor was deprecated - but we want to support lower API levels.
-       super("com.example.activity", DeckardActivity.class);
-     }
+    public DeckardEspressoTest() {
+        // This constructor was deprecated - but we want to support lower API levels.
+        super("com.example.activity", DeckardActivity.class);
+    }
+
     @Override
     public void setUp() throws Exception {
         super.setUp();
@@ -27,6 +28,6 @@ public class DeckardEspressoTest extends ActivityInstrumentationTestCase2<Deckar
 
     public void testCheckText() {
         onView(withId(R.id.text))
-            .check(matches(withText("Hello Espresso!")));
-      }
+                .check(matches(withText("Hello Espresso!")));
+    }
 }
