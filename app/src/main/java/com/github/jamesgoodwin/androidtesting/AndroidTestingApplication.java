cff6ca8dcd4ea3132a7ed5c2e289aa32418c0d0c
James Goodwin <jamesgoodwin@gmail.com>
2015-01-20 22:30:47 +0000

    Trying out the use of IdlingResource when loading activities

---
+++ app/src/main/java/com/github/jamesgoodwin/androidtesting/AndroidTestingApplication.java
@@ -0,0 +1,7 @@
+package com.github.jamesgoodwin.androidtesting;
+
+import android.app.Application;
+
+public class AndroidTestingApplication extends Application {
+
+}
--- robolectric-tests/src/test/java/com/example/activity/DeckardActivityRobolectricTest.java
+++ robolectric-tests/src/test/java/com/example/activity/DeckardActivityRobolectricTest.java
@@ -3,20 +3,19 @@ package com.example.activity;
 import android.app.Activity;
 
 import com.example.RobolectricGradleSubModuleTestRunner;
-import com.example.robolectric.DeckardActivity;
+import com.github.jamesgoodwin.androidtesting.LoadingActivity;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 
-
 @RunWith(RobolectricGradleSubModuleTestRunner.class)
 public class DeckardActivityRobolectricTest {
 
     @Test
     public void testSomething() throws Exception {
-        Activity activity = Robolectric.buildActivity(DeckardActivity.class).create().get();
+        Activity activity = Robolectric.buildActivity(LoadingActivity.class).create().get();
         Assert.assertTrue(activity != null);
     }
 }
